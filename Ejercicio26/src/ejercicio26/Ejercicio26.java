c398eb0a1c8f8f5b2426cf8ad19b1833b7dde1f7
arturo <arturo@ArturoMSI>
2018-04-18 09:22:36 +0200

    Ejercicio Finalizado

---
+++ Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -0,0 +1,16 @@
+package ejercicio26;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz i=new Interfaz();
+    }
+
+}
